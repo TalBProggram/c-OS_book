4585c99d0d852fe5946fdfea0c1348185b00743c
TalBProggram <65766191+TalBProggram@users.noreply.github.com>
2021-09-28 19:17:41 +0300

    Add files via upload

--- syscall31.cpp
+++ syscall31.cpp
@@ -9,39 +9,40 @@ int main()
 {
 	HANDLE myFile = CreateFileA
 	(
-		"testText.txt",
+		"testText.txt", // file name 
 		GENERIC_READ,
-		0,
+		0, //default
 		NULL,
 		OPEN_EXISTING,
 		FILE_ATTRIBUTE_NORMAL,
 		NULL
 	);
-	if (myFile == INVALID_HANDLE_VALUE)
+	if (myFile == INVALID_HANDLE_VALUE) // if cant didnt open file
 	{
 		printf("failed to open file");
-	}
+	} 
 
-	//TCHAR buffr[20];
+	
 	constexpr size_t bufSize = 256;
 	char buffr[bufSize];
 	DWORD dwbytestoread = 255;
 	DWORD dwbytesread = 0;
 
-	BOOL secc = ReadFile(
-		myFile,
-		(void*)buffr,
-		dwbytestoread,
+	BOOL secc = ReadFile( 
+		myFile,		//handle
+		(void*)buffr,	 // buffer
+		dwbytestoread, 
 		&dwbytesread,
-		NULL);
+		NULL
+	);
 
-	if (dwbytesread > 0)
+	if (dwbytesread > 0) // if it has read the file 
 	{
-		buffr[dwbytesread] = '\0';
+		buffr[dwbytesread] = '\0'; // remove null at the end (null terminator)
 		cout <<"file content: " << buffr << endl;
 	}
 
-	CloseHandle(myFile);
+	CloseHandle(myFile); // close 
 	return 0;
 }
 
